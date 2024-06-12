e183f127bf7cebb76f4abdf77d7b7db97423305e
Maximiliano Pineda Francolugo <maximiliano.pinedafr@ucalgary.ca>
2024-06-11 18:13:34 -0600

    Added the testing environment to scenes

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    public int numberOfViews;
+    //private ArrayList<>
     public void PrintMessage()
     {
         Debug.Log("Got clicked");
     }
+
+
 }
