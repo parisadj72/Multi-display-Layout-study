18ee71e3aaf7ff325ec7b10c5581e1c655e0cb3d
Maximiliano Pineda Francolugo <maximiliano.pinedafr@ucalgary.ca>
2024-06-20 13:31:48 -0600

    Added script for experiment 1. Still working on instantiating layouts.

---
+++ Assets/Scripts/Instantiate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour
+{
+    public GameObject InstantiatePrefab;
+    public Transform location;
+    private void Awake()
+    {
+        
+        Instantiate(InstantiatePrefab, this.transform);
+    }
+}
